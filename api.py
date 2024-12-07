ebf2947737390f72f2885f1a9d556c13f45afb0e
Palita-hub <palita6655@gmail.com>
2024-12-07 20:06:53 +0700

    Update api.py

--- api.py
+++ api.py
@@ -40,12 +40,16 @@ def get_word_details(word):
 
             for i, block in enumerate(meaning_blocks[1:]):
                 meaning_index = block.find(":")
+                
                 synonyms_index = block.find('Synonyms:')                
+                
                 example_index = block.find('Example:')
+                
                 meaning = block[meaning_index + 1:block.find("Synonyms:")].strip()
-                synonyms1 = block[synonyms_index+7:].strip()
+                synonyms1 = block[block.find['.']:example_index].strip()
                 synonyms2 = synonyms1.split('.')
                 synonyms3 = (synonyms2[0]).replace(',','\n')
+                example = block[synonyms3.find['.']:].strip()                
                 synonyms =synonyms3.split('\n')
                 example = block[example_index + 1:].strip()
                 rows.append({"Word": word, "Meaning": meaning, "Synonyms": synonyms,'Example sentence': example})
