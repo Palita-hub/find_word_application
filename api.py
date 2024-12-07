7f9d2371fb68c561545e073ce718ea4a894fc7fc
Palita-hub <palita6655@gmail.com>
2024-12-07 18:01:06 +0700

    Update api.py

--- api.py
+++ api.py
@@ -42,8 +42,8 @@ def get_word_details(word):
                 meaning = block[meaning_index + 1:block.find("Synonyms:")].strip()
                 synonyms1 = block[block.find("Synonyms:") + len("Synonyms:"):].strip()
                 synonyms2 = synonyms1.replace(',','\n')
-                synonyms3 = synonyms2.remove('.')
-                synonyms =synonyms2.split('\n')
+                synonyms3 = synonyms2.replace('.','')
+                synonyms =synonyms3.split('\n')
                 rows.append({"Word": word, "Meaning": meaning, "Synonyms": synonyms})
 
             df = pd.DataFrame(rows)
