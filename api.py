8671071ea6722f1371f181605e2c65f53152ad75
Palita-hub <palita6655@gmail.com>
2024-12-07 23:27:53 +0700

    Update api.py

--- api.py
+++ api.py
@@ -42,12 +42,12 @@ def get_word_details(word):
                 meaning_index = block.find(":")
                 synonyms_index = block.find('.')
                 meaning = block[meaning_index + 1:block.find("Synonyms:")].strip()
-                synonyms1 = block[block.find("Synonyms:") + len("Synonyms:"):].strip()
+                synonyms1 = block[block.find("Synonyms:") + len("Synonyms:"):block.find('Example:'-1)].strip()
                 synonyms2 = synonyms1.replace(',','\n')
                 synonyms3 = synonyms2.split('.')
                 synonyms4 = synonyms3[0]
                 synonyms =synonyms4.split('\n')
-                no_synonyms = block[block.find("Synonyms:") + len("Synonyms:")-1:block.find('Example:')-1].strip()
+                #no_synonyms = block[block.find("Synonyms:") + len("Synonyms:")-1:block.find('Example:')-1].strip()
                 #if no_synonyms == ':':
                     #synonyms = 'No synonyms found'
                 example = block[block.find('Example:')+len('Example:'):].strip()   
